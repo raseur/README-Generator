136fdee95148094e9a1a42997f232d2b4f90ba7b
Antoine J <64862896+raseur@users.noreply.github.com>
2024-03-18 00:08:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
